dc9fbac3aa0884b35befc0927d418a3a9c5870d4
MMC-Scholars <latrunkster@gmail.com>
2017-11-09 15:00:29 -0500

    Recreated Program. Compiles now. Restructuring goal system. -Joy

---
+++ Source/MMC_Skee_Ball/MyClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class MMC_SKEE_BALL_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
